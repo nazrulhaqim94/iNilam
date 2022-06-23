b4155994a0c3adf0dbd4c598c7f00c8c6e9fd46a
middi11 <hamidi.hasmar94@gmail.com>
2022-06-23 20:02:03 +0800

    change base url in settings.py Commit

--- main/settings.py
+++ main/settings.py
@@ -18,7 +18,8 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_URL = 'http://localhost:8000/'
+# BASE_URL = 'http://localhost:8000/'
+BASE_URL = 'https://inilam.herokuapp.com/'
 
 
 # Quick-start development settings - unsuitable for production
